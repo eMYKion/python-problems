442340158fb77e99e1d068da019d78b586d4b9f9
Mayank Mali <emykion@gmail.com>
2023-07-31 12:49:48 -0400

    add palindrome problem

---
+++ str-palindrome.py
@@ -0,0 +1,12 @@
+'''
+A palindrome is a string that is the same forward as it is backward.
+Write a function is_palindrome that takes a string s and returns True
+if and only if s is a palindrome.
+'''
+
+def is_palindrome(s):
+    pass
+
+#ignore this code
+def solution(*args):
+    return is_palindrome(*args)
---
+++ test/cases-str-palindrome.py
@@ -0,0 +1,10 @@
+cases = {
+    ("",): True,
+    ("a",): True,
+    ("aa",): True,
+    ("aba",): True,
+    ("ab",): False,
+    ("abc",): False,
+    ("abaabaaba",): True,
+    ("bbabaabbaababb",): True,    
+}
